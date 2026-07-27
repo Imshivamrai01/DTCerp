2b987db365e17bcdea5e57f4b9cee2676c333431
Shivam Rai <shivamworld8090@gmail.com>
2026-07-27 14:50:11 +0530

    Update Next.js to fix security vulnerabilities

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ihnauuYZn02+Vw+4EPIJoXPSZnLiWd4clN/mHA6SFyE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
