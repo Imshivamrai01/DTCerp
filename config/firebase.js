fe535dbe98d989d1f21b0cedf2e34ec5764ee484
Shivam Rai <shivamworld8090@gmail.com>
2026-07-27 14:08:52 +0530

    Remove Firebase integration to fix Vercel build

--- config/firebase.js
+++ config/firebase.js
@@ -1,23 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIRE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIRE_AUTH_DOM,
-  // databaseURL: process.env.NEXT_PUBLIC_FIRE_DB_URL,
-  projectId: process.env.NEXT_PUBLIC_FIRE_PRJ_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIRE_STG_BKT,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIRE_MSG_ID,
-  appId: process.env.NEXT_PUBLIC_FIRE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIRE_MES_ID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const auth = null;
+export const db = null;
+export const storage = null;
